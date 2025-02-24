52e9ff8c1712435536e9f31a57317f26d0c2d165
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2025-02-24 23:15:35 +0200

    feat: Leaderboard podium table row colors, table row onClick highlight colors

--- src/app/_components/Leaderboard.tsx
+++ src/app/_components/Leaderboard.tsx
@@ -6,31 +6,35 @@ import {
   type NormalizedLeaderboardList,
 } from "@/server/api/routers/leaderboard";
 
-import { use, useState } from "react";
+import { use, useEffect, useState } from "react";
 import { format } from "date-fns";
 import { diffFromNow, longDateFormat } from "../helpers/dateFormatHelpers";
 
 /**
- * Helper function to convert milliseconds to MM:SS.000 format
+ * Helper function to convert milliseconds to mm:ss.SSS format
  */
 function millisecondsToLaptime(milliseconds: number): string {
-  console.log("[millisecondsToLaptime] input:", milliseconds);
   const date = new Date(milliseconds);
-  const formattedTime = format(date, "mm:ss.SSS");
-  console.log("[millisecondsToLaptime] output:", formattedTime);
-  return formattedTime;
+  return format(date, "mm:ss.SSS");
 }
 
-const TableData: FC<{ children: ReactNode; className?: string }> = ({
-  children,
-  className,
-}) => (
-  <td
-    className={`border-r border-gray-700 p-2 px-4 font-mono text-base font-semibold ${className}`}
-  >
-    {children}
-  </td>
-);
+const TableData: FC<{
+  children: ReactNode;
+  className?: string;
+  isHighlighted?: boolean;
+}> = ({ children, className, isHighlighted }) => {
+  const highlightClasses = isHighlighted
+    ? "border-y border-y-table-highlighted-row-border first:border-l first:border-l-table-highlighted-row-border last:border-r last:border-r-table-highlighted-row-border bg-table-highlighted-row-bg"
+    : "last:border-r-0";
+
+  return (
+    <td
+      className={`text-shadow-sm border-r border-gray-700 px-3 py-[10px] font-mono text-base font-semibold first:rounded-l-lg last:rounded-r-lg ${className} ${highlightClasses}`}
+    >
+      {children}
+    </td>
+  );
+};
 
 type RaceResultsProps = {
   results: Promise<NormalizedLeaderboardList | null>;
@@ -41,6 +45,9 @@ export default function Leaderboard({ results }: RaceResultsProps) {
   const tabs = ["Driver best", "All"] as const;
   const [activeTab, setActiveTab] =
     useState<(typeof tabs)[number]>("Driver best");
+  const [highlightedRowIndex, setHighlightedRowIndex] = useState<number | null>(
+    null,
+  );
 
   if (!unsortedResults || unsortedResults.length === 0) {
     return (
@@ -71,6 +78,23 @@ export default function Leaderboard({ results }: RaceResultsProps) {
   const tableData =
     activeTab === "Driver best" ? resultsByDriverBest : resultsSorted;
 
+  const handleRowClick = (index: number) => {
+    if (activeTab === "Driver best" && index < 3) {
+      return;
+    }
+
+    if (highlightedRowIndex === index) {
+      setHighlightedRowIndex(null);
+      return;
+    }
+
+    setHighlightedRowIndex(index);
+  };
+
+  useEffect(() => {
+    setHighlightedRowIndex(null);
+  }, [activeTab]);
+
   return (
     <div>
       {/* TAB SWITCHER */}
@@ -93,7 +117,7 @@ export default function Leaderboard({ results }: RaceResultsProps) {
 
       <div className="min-h-screen w-screen overflow-x-auto">
         <table className="mx-4 min-w-[600px] table-auto border-separate border-spacing-y-2 whitespace-nowrap">
-          <thead className="sticky top-0">
+          <thead>
             <tr>
               <th className="px-4 text-start">#</th>
               <th className="px-4 text-start">Driver</th>
@@ -103,7 +127,7 @@ export default function Leaderboard({ results }: RaceResultsProps) {
               <th className="px-4 text-start">Date</th>
             </tr>
           </thead>
-          <tbody className="">
+          <tbody>
             {tableData.map((result, index) => {
               const lapTimeInMsInt = parseInt(result.laptimeInMs);
               const laptimeFormattedDuration =
@@ -122,20 +146,55 @@ export default function Leaderboard({ results }: RaceResultsProps) {
               const sinceAddedDateString = diffFromNow(result.dateAdded);
               const formattedDateAdded = longDateFormat(result.dateAdded);
 
+              const podiumColors = [
+                "bg-podium-gold",
+                "bg-podium-silver",
+                "bg-podium-bronze",
+              ];
+
+              const defaultRowBgClass = "bg-gray-800";
+              const podiumColor =
+                activeTab === "Driver best"
+                  ? (podiumColors[index] ?? defaultRowBgClass)
+                  : defaultRowBgClass;
+
+              const isHighlighted = (() => {
+                if (activeTab === "Driver best" && index < 3) {
+                  return false;
+                }
+
+                if (highlightedRowIndex === null) {
+                  return false;
+                }
+
+                return highlightedRowIndex === index;
+              })();
+
               return (
-                <tr key={index} className="border border-gray-700 bg-gray-800">
-                  <TableData className="rounded-l-lg px-3 text-center">
+                <tr
+                  key={index}
+                  onClick={() => handleRowClick(index)}
+                  className={`${podiumColor} border-table-highlighted-row-border`}
+                >
+                  <TableData
+                    isHighlighted={isHighlighted}
+                    className="text-center"
+                  >
                     {index + 1}
                   </TableData>
-                  <TableData>{result.driverName}</TableData>
-                  <TableData>{laptimeFormattedDuration}</TableData>
-                  <TableData>
+                  <TableData isHighlighted={isHighlighted}>
+                    {result.driverName}
+                  </TableData>
+                  <TableData isHighlighted={isHighlighted}>
+                    {laptimeFormattedDuration}
+                  </TableData>
+                  <TableData isHighlighted={isHighlighted}>
                     {index === 0 ? "-" : formattedGapToLeader}
                   </TableData>
-                  <TableData>
+                  <TableData isHighlighted={isHighlighted}>
                     {index === 0 ? "-" : formattedGapToNext}
                   </TableData>
-                  <TableData className="rounded-r-lg border-none">
+                  <TableData isHighlighted={isHighlighted}>
                     {sinceAddedDateString} · {formattedDateAdded}
                   </TableData>
                 </tr>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,11 @@ export default {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      textShadow: {
+        sm: "1px 1px 2px rgba(0, 0, 0, 0.5)",
+        DEFAULT: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+        lg: "3px 3px 6px rgba(0, 0, 0, 0.5)",
+      },
       colors: {
         "list-item-bg": "rgba(255, 255, 255, 0.05)",
         "list-item-border": "#404040",
@@ -14,6 +19,8 @@ export default {
         "list-item-scrim-start": "rgba(0, 0, 0, 0.3)",
         "list-item-scrim-end": "rgba(0, 0, 0, 0.5)",
         subtitle: "#B3B3B3",
+        "table-highlighted-row-bg": "rgba(233, 255, 86, 0.15)",
+        "table-highlighted-row-border": "#E7E739",
       },
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
@@ -27,6 +34,14 @@ export default {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         "gradient-linear": "linear-gradient(var(--tw-gradient-stops))",
       },
+      backgroundImage: {
+        "podium-gold":
+          "linear-gradient(to bottom, #F9EC7B 0%, #C48A1E 40%, #EAB81A 100%)",
+        "podium-silver":
+          "linear-gradient(to bottom, #E4E4E4 0%, #6C6C6C 40%, #C2C2C2 100%)",
+        "podium-bronze":
+          "linear-gradient(to bottom, #F4B08B 0%, #985125 40%, #F07F4A 100%)",
+      },
     },
   },
   plugins: [
@@ -39,6 +54,15 @@ export default {
         ".mask-image-gradient-linear": {
           "mask-image":
             "linear-gradient(to bottom, rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 0) 100%)",
+        },
+        ".text-shadow-sm": {
+          textShadow: "1px 1px 2px rgba(0, 0, 0, 0.5)",
+        },
+        ".text-shadow": {
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+        },
+        ".text-shadow-lg": {
+          textShadow: "3px 3px 6px rgba(0, 0, 0, 0.5)",
         },
       });
     },
