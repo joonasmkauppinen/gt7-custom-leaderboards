59be34af04b47668ddef2cf4a4235e16df514c36
Joonas Kauppinen <joonasm.kauppinen@gmail.com>
2025-03-02 12:12:57 +0200

    chore: remove commented out env variables

--- src/env.js
+++ src/env.js
@@ -7,21 +7,14 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
+    ADMIN_PASSWORD: z.string(),
+    ADMIN_USERNAME: z.string(),
+    AUTH_SECRET: z.string(),
+    GCP_PRIVATE_KEY: z.string(),
+    GCP_PROJECT_ID: z.string(),
+    GCP_SERVICE_ACCOUNT_EMAIL: z.string(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     SHEET_ID: z.string(),
-    ADMIN_USERNAME: z.string(),
-    ADMIN_PASSWORD: z.string(),
-    AUTH_SECRET: z.string(),
-    GCP_PROJECT_ID: z.string(),
-    // GCP_PRIVATE_KEY_ID: z.string(),
-    GCP_PRIVATE_KEY: z.string(),
-    GCP_SERVICE_ACCOUNT_EMAIL: z.string(),
-    // GCP_CLIENT_ID: z.string(),
-    // GCP_CLIENT_X509_CERT_URL: z.string(),
-    // GCP_AUTH_URI: z.string(),
-    // GCP_TOKEN_URI: z.string(),
-    // GCP_AUTH_PROVIDER_X509_CERT_URL: z.string(),
-    // GCP_UNIVERSE_DOMAIN: z.string(),
   },
 
   /**
@@ -38,23 +31,14 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    NODE_ENV: process.env.NODE_ENV,
-    SHEET_ID: process.env.SHEET_ID,
-    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
     ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+    ADMIN_USERNAME: process.env.ADMIN_USERNAME,
     AUTH_SECRET: process.env.AUTH_SECRET,
-    GCP_SERVICE_ACCOUNT_EMAIL: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
     GCP_PRIVATE_KEY: process.env.GCP_PRIVATE_KEY,
     GCP_PROJECT_ID: process.env.GCP_PROJECT_ID,
-    // GCP_AUTH_PROVIDER_X509_CERT_URL:
-    //   process.env.GCP_AUTH_PROVIDER_X509_CERT_URL,
-    // GCP_AUTH_URI: process.env.GCP_AUTH_URI,
-    // GCP_CLIENT_ID: process.env.GCP_CLIENT_ID,
-    // GCP_CLIENT_X509_CERT_URL: process.env.GCP_CLIENT_X509_CERT_URL,
-    // GCP_PRIVATE_KEY_ID: process.env.GCP_PRIVATE_KEY_ID,
-    // GCP_TOKEN_URI: process.env.GCP_TOKEN_URI,
-    // GCP_UNIVERSE_DOMAIN: process.env.GCP_UNIVERSE_DOMAIN,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    GCP_SERVICE_ACCOUNT_EMAIL: process.env.GCP_SERVICE_ACCOUNT_EMAIL,
+    NODE_ENV: process.env.NODE_ENV,
+    SHEET_ID: process.env.SHEET_ID,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
